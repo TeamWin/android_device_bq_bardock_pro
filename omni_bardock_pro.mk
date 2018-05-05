1a72bb9f42a971d243e0b1fea17360939429016f
Kra1o5 <kra1o5x@gmail.com>
2018-05-06 01:06:38 +0200

    Update to O MR1
    Change-Id: If944fbc7ffbd97389b07bc44c6a3db6f9d83e7ed

--- omni_bardock_pro.mk
+++ omni_bardock_pro.mk
@@ -6,9 +6,7 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata \
-    device/bq/bardock_pro/kernel:kernel
+PRODUCT_COPY_FILES += device/bq/bardock_pro/kernel:kernel
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := bardock_pro
